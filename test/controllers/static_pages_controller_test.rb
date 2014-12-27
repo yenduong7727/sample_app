b2ea7942d6ab91e0b941cbf4b9b2e441e03ff6cc
yenduong7727 <yenduong7727@gmail.com>
2014-12-27 02:05:26 +0000

    Eliminate repetition (solves exercise 3.1)

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,24 +1,30 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  
+  # self-defined func to git rid of duplication in title test
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
   # gets an action and verifies (via an assertion) that the result is a success. home and help action
   # Here the use of get indicates that our tests expect the Home and Help pages to be ordinary web pages, accessed using a GET request
 
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About | #{@base_title}"
   end
 end
